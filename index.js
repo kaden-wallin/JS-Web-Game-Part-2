3709363d08ebd197bc65e86ce55bdc8bae9c1466
BuildTools <unconfigured@null.spigotmc.org>
2022-10-03 21:03:35 -0600

    Made the items clickable and able to show up in the inventory

--- index.js
+++ index.js
@@ -10,10 +10,18 @@ function newImage(url, left, bottom){
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
+    
+    item.addEventListener('dblclick', function() {
+        item.remove()
+        let inventoryItem = document.createElement('img')
+        inventoryItem.src = url
+        inventory.append(inventoryItem)
+    })
 }
+let inventory
 
 function newInventory(){
-    let inventory = document.createElement('div')
+    inventory = document.createElement('div')
     inventory.style.position = 'fixed'
     inventory.style.bottom = '0px';
     inventory.style.left = '0px'
